10ebc4021f9b2227655056086a4a277039453aa8
cavigna <40524472+cavigna@users.noreply.github.com>
2022-03-14 12:13:38 -0300

    Added Predict your age! 7 kyu

--- src/main/kotlin/kyu_7.kt
+++ src/main/kotlin/kyu_7.kt
@@ -1,3 +1,4 @@
+import kotlin.math.sqrt
 import kotlin.test.assertEquals
 
 fun evaporator(content: Double, evap_per_day: Double, threshold: Double): Int {
@@ -459,9 +460,38 @@ fun hidePasswordFromConnection(urlString: String): String {
     }
 }
 
+
+
+/**
+ *Predict your age!
+
+ *My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+ *
+ */
+
+//shorter ==> fun predictAge(vararg age:Int) = (Math.sqrt((age.map{it*it}.sum().toDouble()))/2).toInt()
+
+fun predictAge(age1: Int, age2: Int, age3: Int, age4: Int, age5: Int, age6: Int, age7: Int, age8: Int): Int{
+    val edades = mutableListOf(age1, age2, age3, age4, age5, age6, age7, age8)
+    edades.forEachIndexed { index, i ->
+        edades[index] *= i
+    }
+    println((sqrt(edades.sum().toDouble())/2).toInt())
+    return (sqrt(edades.sum().toDouble())/2).toInt()
+}
+
 fun main() {
-    assertEquals(
-        "jdbc:mysql://sdasdasdasd:szdasdasd:dfsdfsdfsdf/sdfsdfsdf?user=root&password=*****",
-        hidePasswordFromConnection("jdbc:mysql://sdasdasdasd:szdasdasd:dfsdfsdfsdf/sdfsdfsdf?user=root&password=12345")
-    )
+    assertEquals(86, predictAge(65, 60, 75, 55, 60, 63, 64, 45))
+    assertEquals(79, predictAge(32, 54, 76, 65, 34, 63, 64, 45))
 }

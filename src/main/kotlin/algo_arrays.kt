88a9d95efe8f77340d953d2280d31f69a39d30ba
cavigna <40524472+cavigna@users.noreply.github.com>
2022-03-29 16:40:04 -0300

    Added Array Algorithm for Interviews

--- src/main/kotlin/algo_arrays.kt
+++ src/main/kotlin/algo_arrays.kt
@@ -4,7 +4,7 @@ import kotlin.random.Random
  * How do you find the missing number in a given integer array of 1 to 100?
  */
 
-fun findMissing(list: List<Int>):Int{
+fun findMissing(list: List<Int> = (0..100).toMutableList().also { it.remove(23) }.toList()):Int{
 
     val total = (0..100).toList().sum()
 
@@ -15,11 +15,29 @@ fun findMissing(list: List<Int>):Int{
     return if(res == total) 0 else res
 }
 
-fun main(){
-    val prueba = (0..100).toMutableList().also { it.remove(23) }.toList()
-    findMissing(
-        (0..100).toMutableList().also { it.remove(Random.nextInt(0,100)) }.toList()
-    )
+/**
+ * How do you find the duplicate number on a given integer array?
+ */
 
-    findMissing(prueba)
+fun duplicateInt(lista: List<Int> = listOf(-23,140,-93,16699,3,12,18,55,3)): Int{
+    val listaOrdenada = lista.sorted()
+
+    var res = 0
+
+    for (i in 0 until listaOrdenada.size-1){
+        if (lista[i] == lista[i+1])  {
+            res = lista[i] }
+    }
+
+    println(println(res))
+    return res
+}
+
+fun woordMatch(word:String, list: List<String>){
+    
+}
+
+fun main(){
+
+    duplicateInt()
 }
